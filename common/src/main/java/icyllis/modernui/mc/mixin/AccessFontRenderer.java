f74f82cfa8b9709ad205cb3e2dd27376993c3da3
kibi <furry@furry.coffee>
2026-04-05 21:25:32 +0000

    removed unused imports that casued build to break

--- common/src/main/java/icyllis/modernui/mc/mixin/AccessFontRenderer.java
+++ common/src/main/java/icyllis/modernui/mc/mixin/AccessFontRenderer.java
@@ -19,9 +19,7 @@
 package icyllis.modernui.mc.mixin;
 
 import net.minecraft.client.gui.Font;
-import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Deprecated
 @Mixin(Font.class)
